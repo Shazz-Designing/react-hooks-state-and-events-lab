575fa15194e4ead0e190ab722fe805ba0b4898e1
sharon <sapot@safaricom.co.ke>
2023-11-13 20:19:55 +0300

    integrate the filter functionality into your ShoppingList component

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,11 +1,25 @@
-import React from "react";
+import React, { useState } from 'react';
 import Item from "./Item";
 
 function ShoppingList({ items }) {
+
+  //Create state variable for selected category
+  const [selectedCategory, setSelectedCategory] = useState('All'); // Default to 'All'
+
+  //Create event handler for select change
+  const handleCategoryChange = (event) => {
+    setSelectedCategory(event.target.value);
+  };
+
+  //Filter items based on selected category
+  const filteredItems = selectedCategory === 'All' 
+    ? items 
+    : items.filter(item => item.category === selectedCategory);
+
   return (
     <div className="ShoppingList">
       <div className="Filter">
-        <select name="filter">
+        <select name="filter" value={selectedCategory} onChange={handleCategoryChange}>
           <option value="All">Filter by category</option>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
@@ -13,7 +27,7 @@ function ShoppingList({ items }) {
         </select>
       </div>
       <ul className="Items">
-        {items.map((item) => (
+        {filteredItems.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
         ))}
       </ul>
