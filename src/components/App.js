53f43c021e78ae3aa48c91ebe487ab211ab20064
sharon <sapot@safaricom.co.ke>
2023-11-13 16:03:49 +0300

    Create a state variable for dark mode

--- src/components/App.js
+++ src/components/App.js
@@ -3,9 +3,9 @@ import ShoppingList from "./ShoppingList";
 import itemData from "../data/items";
 
 function App() {
-
-  // replace 'false' with a state variable that can be toggled between true and false
-  // this will be used for the Dark Mode Toggle feature
+  //Create a state variable for dark mode
+  const [isDarkMode, setDarkMode] = useState(false);
+  
   const appClass = false ? "App dark" : "App light"
 
   return (
